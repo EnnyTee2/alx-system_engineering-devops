07fc17d5eb008f3ee78f7a166efc6aa1112aeb51
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-26 16:52:47 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,39 +1,8 @@
-# add stable version of nginx
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# update software packages list
-exec { 'update packages':
-  command => 'sudo apt update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# install nginx
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Adding the header to config file
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content => "\
-  server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        # Add Custom HTTP Response Header
-        add_header X-Served-By $HOSTNAME;
-        
-        server_name _;
-        }
-}"
-}
-
-# restart nginx
-exec { 'restart service':
-  command => 'service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
