abe5cf816f76dfbc1e5afc83835a5acd07a5097a
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-26 16:34:58 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,29 +15,7 @@ package { 'nginx':
   ensure     => 'installed',
 }
 
-exec { 'allow HTTP':
-  command => "ufw allow 'Nginx HTTP'",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
-}
-
-# change folder rights
-exec { 'chmod www folder':
-  command => 'chmod -R 755 /var/www',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# create index file
-file { '/var/www/html/index.html':
-  content => "Hello World!\n",
-}
-
-# create index file
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-# add redirection and error page
+# add custom http response header
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
@@ -46,24 +24,9 @@ file { 'Nginx default config file':
         listen 80 default_server;
         listen [::]:80 default_server;
         # Add Custom HTTP Response Header
-        add_header "X-Served-By" $HOSTNAME;
-
-        root /var/www/html;
-
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
+        add_header X-Served-By $HOSTNAME;
 
         server_name _;
-
-        location /redirect_me {
-        return 301 http://google.com/;
-        }
-
-        error_page 404 /404.html;
-
-        location /404 {
-          root /var/www/html;
-          internal;
         }
 }
 ",
