ec3f7b0b857e9f439864028723be598ea5fc9a8d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 13:31:49 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,13 +12,12 @@ def recurse(subreddit, hot_list=[], after=None):
         64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404 or response.status_code == 301:
+    if response.status_code == 404:
         return None
     results = response.json().get("data").get("children")
     after = response.json().get('after')
     for res in results:
         hot_list.append(res.get('data').get('title'))
     if after is not None:
-        recurse(subreddit, hot_list=hot_list, after=after)
-    else:
-        return hot_list
+        return recurse(subreddit, hot_list=hot_list, after=after)
+    return hot_list
