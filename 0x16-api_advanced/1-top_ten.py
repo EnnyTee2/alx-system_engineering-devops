ebb3bae9d10e396957f2215c78793e3b57c5af89
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 14:34:43 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -22,7 +22,7 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
     [print(c.get("data").get("title")) for c in results.get("children")]
 
 
-""""def number_of_subscribers(subreddit):
+"""def number_of_subscribers(subreddit):
     """prints the titles of the first 10 hot
        posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
