a8f421f06621cea82e1afd8362a40acf99688d8f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 11:57:52 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,10 +4,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """prints the titles of the first 10 hot
+       posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_\
+        64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
     }
     params = {
         "limit": 10
