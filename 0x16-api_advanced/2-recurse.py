0dd41ff852922c6ef8eb4bcc6522798a8c96dce2
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 14:13:12 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,16 +11,13 @@ def recurse(subreddit, hot_list=[], after=''):
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_\
         64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
     }
-    param = {
-        "after": after
-    }
     if after == '':
         response = requests.get(url, headers=headers, allow_redirects=False)
     else:
-        response = requests.get(url, headers=headers, params=param, allow_redirects=False)
+        response = requests.get(url, headers=headers, after=after, allow_redirects=False)
     if response.status_code == 404:
         return None
-    results = response.json().get("data").get("children")
+    results = response.json().get("data").
     aft = response.json().get('data').get('after')
     for res in results:
         hot_list.append(res.get('data').get('title'))
