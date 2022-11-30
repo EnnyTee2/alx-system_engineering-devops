ff1464a8184ef6db034b64de0f22c932e3b60fff
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 14:38:20 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,11 +4,12 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """prints the titles of the first 10 hot
+       posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_\
+        64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
     }
     params = {
         "limit": 10
@@ -20,3 +21,4 @@ v1.0.0 (by /u/firdaus_cartoon_jr)"
         return
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
+"""
