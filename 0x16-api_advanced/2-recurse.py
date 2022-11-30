75666b19d57855b9f79da866a8400aac24a483a6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-30 14:24:24 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
+def recurse(subreddit, hot_list=[], after='', count=0):
     """ returns a list containing the titles
     of all hot articles for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
